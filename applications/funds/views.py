cdf420b8f516d710b7f079c3af0d46791354fe4d
Iñaki Retegi <retegi84@gmail.com>
2025-12-13 23:25:14 +0100

    Fund detail y list, transacciones BUY y ajustes de modelos

--- applications/funds/views.py
+++ applications/funds/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render, get_object_or_404
 from .models import Fund
+from applications.transactions.models import Transaction
 
 def fund_list(request):
     funds = Fund.objects.all()
@@ -10,4 +11,19 @@ def fund_list(request):
 
 def fund_detail(request, pk):
     fund = get_object_or_404(Fund, pk=pk)
-    return render(request, "funds/fund_detail.html", {"fund": fund})
+
+    buy_transactions = Transaction.objects.filter(
+        transaction_type="BUY"
+    ).select_related("product")
+
+    return render(request, "funds/fund_detail.html", {
+        "fund": fund,
+        "buy_transactions": buy_transactions,
+    })
+
+def fund_list(request):
+    funds = Fund.objects.all().order_by("name")
+
+    return render(request, "funds/fund_list.html", {
+        "funds": funds
+    })
